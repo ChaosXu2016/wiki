fff9255cbbfb2b799707a993fcc76de500cd6f76
Su <hesu@newbanker.cn>
2018-11-17 13:43:35 +0800

    完成module读书笔记的编写

--- config/su.js
+++ config/su.js
@@ -4,7 +4,8 @@ module.exports = {
     children: [
       ['/docs/su/markdown', 'markdown语法入门'],
       ['/docs/su/debounce', '函数防抖与函数节流'],
-      ['/docs/su/review', '天眼查项目review']
+      ['/docs/su/review', '天眼查项目review'],
+      ['/docs/su/modules', '读ES6modules有感'],
     ]
   }
 }
