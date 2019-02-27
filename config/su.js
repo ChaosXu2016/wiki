87ed8279cfa5288329426e19b5a5194fa54b47a8
Su <hesu@newbanker.cn>
2019-02-27 11:31:24 +0800

    几篇文章的梳理

--- config/su.js
+++ config/su.js
@@ -4,8 +4,13 @@ module.exports = {
     children: [
       ['/docs/su/markdown', 'markdown语法入门'],
       ['/docs/su/debounce', '函数防抖与函数节流'],
+      ['/docs/su/throttle', '再谈函数防抖与函数节流'],
       ['/docs/su/review', '天眼查项目review'],
       ['/docs/su/modules', '读ES6modules有感'],
+      ['/docs/su/async', '理解javascript的async/await'],
+      ['/docs/su/await', 'async/await的执行顺序详解'],
+      ['/docs/su/interview', '从一道题浅说javascript的事件循环'],
+      ['/docs/su/observer', '用观察者模式编写一个可被其他对象拓展的可复用的自定义事件系统']
     ]
   }
 }
